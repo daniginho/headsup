9d9fce6422e845caacdd0d4f8fca2013538b0b65
danielguest <daniginho@outlook.com>
2020-12-24 16:43:33 +0000

    we have stop the countdown moving and put the countdown into the middle

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ three =  [
     [1,1,1,0,0,0],
 ]
 
-# red, yellow, blue, white
+
 red = [204, 0, 0]
 blue = [0, 0, 204]
 white = [255, 255, 255]
@@ -47,14 +47,14 @@ window_height = 540
 
 current_text_colour = possible_colours[0]
 current_background_colour = possible_colours[1]
-box_x = 0 
+box_x = 360-90
+box_y = 270-90
 countdown = 150
 def setup():
     size(window_width, window_height)
-    #no_stroke()
+    
     background(204, 0, 0)
-    #zigBlack = create_font("HussarBoldExtended-PARP.otf", 32)
-    #text_font(zigBlack)    
+       
 
 def draw():
     global time_to_change
@@ -63,7 +63,7 @@ def draw():
     global possible_colours
     global box_x
     global countdown
-
+    global box_y
     if key == "1":
         time_to_change = 30
     elif key == "2":
@@ -78,7 +78,7 @@ def draw():
         time_to_change = 180
 
     if (frame_count % time_to_change) == 0:
-        #print(f"change {frame_count}, {time_to_change} = {frame_count % time_to_change}")
+    
         text_rand = random.randint(0,len(possible_colours) - 1)
         background_rand = random.randint(0,len(possible_colours) - 1)
         if text_rand == background_rand:
@@ -99,31 +99,18 @@ def draw():
         for y in range(0, 6):
             for x in range(0, 6):
                 if letter[y][x]==1:
-                    rect(box_x + (x * box_size), (y * box_size), box_size, box_size )
+                    rect(box_x + (x * box_size),box_y + (y * box_size), box_size, box_size )
 
-    box_x += 1
+    #box_x += 1
     
-    if box_x >= window_width:
-        box_x = 0
-    #text_size(64)
-    #fill(current_text_colour[0], current_text_colour[1], current_text_colour[2])
-    #text_align("CENTER")
-    #text("COLOUR", (window_width/2, window_height/2))
-
-    #if mouse_is_pressed:
-    #    fill(random_uniform(255), random_uniform(127), random_uniform(51), 127)
-    #else:
-    #    fill(255, 15)
-
-    #circle_size = random_uniform(low=10, high=80)
-
-    #circle((mouse_x, mouse_y), circle_size)
+    #if box_x >= window_width:
+    #    box_x = 0
 
 
 def key_pressed(event):
-    #background(204)
+    
     pass
 
-#run(frame_rate=1)
+
 run(frame_rate=60)
 
